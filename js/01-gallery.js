6d341d6816f3a700dcd670f2646d1ee43f7490e9
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-08-29 22:23:33 +0200

    upd
    upd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+
+const liArray = galleryItems.map(({ preview, original, description }) => {
+    return `
+  <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+
+  `;
+
+  
+});
+
+
+const markup = liArray.join('');
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+
+
+
+
