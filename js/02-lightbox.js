77cc9ea7ed730438b51556a4fbb1c8b18f917161
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-08-30 23:50:26 +0200

    upd
    upd

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const liArray = galleryItems.map(({ preview, original, description }) => {
+  return `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      
+
+      
+    />
+  </a>
+</li>
+
+  `;
+});
+
+const markup = liArray.join("");
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+gallery.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    if (event.target.classList.contains("gallery__image")) {
+         const originalImageUrl = event.target.getAttribute('href');
+    const imageDescription = event.target.getAttribute('alt');
+
+        const lightbox = new SimpleLightbox('.gallery a', {
+      animationSpeed: 250,
+            captions: true, 
+            
+           
+            captionsData: imageDescription,
+            captionPosition: 'bottom',
+           
+            
+
+        });
+        
+        lightbox.open(originalImageUrl);
+  }
+});
